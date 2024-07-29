82072a86f34da279d2998b1507b929f52279f000
Richardson <ricardoelias556@gmail.com>
2024-07-29 10:43:38 -0600

    first commit

--- src/componentes/testimonio.js
+++ src/componentes/testimonio.js
@@ -10,8 +10,10 @@ src={require(`../imagenes/${props.imagen}.png`)}
 alt='Foto de emma'/>
 
 <div className='contenedor-texto-testimonio'>
-<p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
-<p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+<p className='nombre-testimonio'>
+   <strong>{props.nombre}</strong> en {props.pais}</p>
+<p className='cargo-testimonio'>{
+props.cargo} en <strong>{props.empresa}</strong></p>
 <p className='texto-testimonio'>"{props.testimonio}"</p>
 </div>
 </div>
